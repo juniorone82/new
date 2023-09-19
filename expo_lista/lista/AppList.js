f5bda92aba5e64e639d0e480a624d182328cbb5c
juniorone82 <g_barker17@hotmail.com>
2023-09-19 19:49:01 -0300

    add files

---
+++ expo_lista/lista/AppList.js
@@ -0,0 +1,19 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+export default function AppList(){
+    return(
+        <View style = {style.container}>
+            <Text>Lista!</Text>
+            <StatusBar style="dark"/>
+        </View>
+    );
+    const style = StyleSheet({
+        container: {
+            flex:1,
+            backgroundColor: '#D93600' ,
+            alignItems: 'center' ,
+            justifyContent: 'center'
+        }
+    })
+}
